2fe26f9c34d28c849b1d2cf315887c5e3106b111
HeinHtetSoe-SCM <scm.heinhtetsoe@gmail.com>
2022-10-03 17:04:16 +0630

    fetching error

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,29 @@
 <template>
-  <Tutorial />
+  <div class="container">
+    <ol>
+      <li v-for="quiz of quizzes" :key="quiz">
+        {{ quiz.title }}
+      </li>
+    </ol>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  data: () => {
+    return {
+      responses: [],
+      quizzes: [],
+      records: [],
+    }
+  },
+  async fetch({ $axios }) {
+    this.responses = await $axios.$get(`http://localhost:8000/api/v1/quiz`);
+    console.log(this.responses.quizzes)
+    for(let i = 0; i < responses.quizzes.length; i++) {
+      this.quizzes.push(responses.quizzes[i]);
+    }
+  },
 }
 </script>
